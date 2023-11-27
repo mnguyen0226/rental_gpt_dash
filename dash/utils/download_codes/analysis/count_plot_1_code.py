bdb998811b60192042dc03ceef130cde7637ca0c
mnguyen0226 <mnguyen0226@vt.edu>
2023-11-27 18:34:41 -0500

    update download code and markdown

--- dash/utils/download_codes/analysis/count_plot_1_code.py
+++ dash/utils/download_codes/analysis/count_plot_1_code.py
@@ -13,27 +13,9 @@ upper_bound = np.percentile(df["price"].values, 99)
 df_filtered = df[df["price"] <= upper_bound]
 
 # create new date columns
-df_filtered["date"] = pd.to_datetime(
-    df_filtered["created"]
-).dt.date  # Returns numpy array of python datetime.date objects.
-df_filtered["year"] = pd.to_datetime(
-    df_filtered["created"]
-).dt.year  # The year of the datetime.
-df_filtered["month"] = pd.to_datetime(
-    df_filtered["created"]
-).dt.month  # The month as January=1, December=12.
-df_filtered["day"] = pd.to_datetime(
-    df_filtered["created"]
-).dt.day  # The day of the datetime.
 df_filtered["hour"] = pd.to_datetime(
     df_filtered["created"]
 ).dt.hour  # The hours of the datetime.
-df_filtered["weekday"] = pd.to_datetime(
-    df_filtered["created"]
-).dt.weekday  # The day of the week with Monday=0, Sunday=6.
-df_filtered["quarter"] = pd.to_datetime(
-    df_filtered["created"]
-).dt.quarter  # The quarter of the date.
 
 # count plot
 fig = plt.figure(figsize=(12, 6))
--- dash/utils/download_codes/analysis/count_plot_3_code.py
+++ dash/utils/download_codes/analysis/count_plot_3_code.py
@@ -1,4 +1,3 @@
-```python
 # import libraries
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -14,20 +13,19 @@ upper_bound = np.percentile(df["price"].values, 99)
 df_filtered = df[df["price"] <= upper_bound]
 
 # create new date columns
-df_filtered["hour"] = pd.to_datetime(
+df_filtered["month"] = pd.to_datetime(
     df_filtered["created"]
-).dt.hour  # The hours of the datetime.
+).dt.month  # The month as January=1, December=12.
 
 # count plot
 fig = plt.figure(figsize=(12, 6))
 sns.countplot(
-    x="hour",
+    x="month",
     hue="interest_level",
     hue_order=["low", "medium", "high"],
     data=df_filtered,
 )
-plt.title("Bar Chart of Interest Level in 24 Hours")
-plt.xlabel("Hour")
+plt.title("Bar Chart of Interest Level in Month")
+plt.xlabel("Month")
 plt.ylabel("Interest Level Counts")
 plt.show()
-```
